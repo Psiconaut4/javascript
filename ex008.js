e70fc7702ef8db09c0a4f36cf91f0c5397d7f4fa
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2024-05-31 22:41:54 -0300

    mais um pouco de js

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`sua velocidade é de ${vel}Km/h`)
+if(vel>60){ 
+    console.log('Você ultrapassou o limite de velocidade e foi multado!')
+}
+console.log('utilize sempre o cinto de segurança!')
---
+++ ex009.js
@@ -0,0 +1,6 @@
+var país = 'Brasil'
+if (país == 'Brasil'){
+    console.log(`Nascendo em ${país}, você é Brasileiro.`)
+} else {
+    console.log(`Nascendo em ${país}, você é estrangeiro.`)
+}
