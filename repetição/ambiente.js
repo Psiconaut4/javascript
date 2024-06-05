5b81bade5404e1fd32bbb1c2c4050b127ca22a10
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2024-06-05 16:10:16 -0300

    estruturas de repetições

---
+++ repetição/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1 
+//Estrutura de repetição com teste lógico no início:
+
+while (c <= 5){
+    console.log(`Ai caramba! ${c}`)
+    c++
+}
+
+/* 
+
+Estrutura de repetição com teste lógico no final:
+do{
+    console.log(`Ai caraho! ${c}`)
+    c++
+} while(c <= 5)
+
+*/
