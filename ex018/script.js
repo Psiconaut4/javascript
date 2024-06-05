a1bcf51d622a29b32514202bb721e9a0979134c2
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2024-06-05 14:41:28 -0300

    identificador de idade

--- ex018/script.js
+++ ex018/script.js
@@ -10,31 +10,38 @@ function verificar(){
         var idade = ano - Number(fano.value)
         var gen = ''
         var img = document.createElement('img')
-        img.setAttribute('id', "foto")
+        img.setAttribute('id', "foto")}
         if (fsex[0].checked){
             gen = 'Homem'
             if (idade >=0 && idade < 12){
                 //criança
+                img.setAttribute('src', 'bebe.m.png')
             } else if (idade >= 12 && idade < 19){
                 //jovem
+                img.setAttribute('src', 'jovem.m.png')
             } else if (idade >=19 && idade < 60){
                 //adulto
+                img.setAttribute('src', 'adulto.m.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'idoso.m.png')
             }
         } else if (fsex[1].checked){
             gen = 'Mulher'
             if (idade >=0 && idade < 12){
                 //criança
+                img.setAttribute('src', 'bebe.f.png')
             } else if (idade >= 12 && idade < 19){
                 //jovem
+                img.setAttribute('src', 'jovem.f.png')
             } else if (idade >=19 && idade < 60){
                 //adulto
+                img.setAttribute('src', 'adulto.f.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'idoso.f.png')
             }
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gen} com ${idade} anos.`
-    }
+        res.innerHTML = `Detectamos ${gen} com ${idade} anos.` 
+        res.appendChild(img)
 }
