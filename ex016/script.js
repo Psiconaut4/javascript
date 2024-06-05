80e1126ef1a6eaf2b9aa352aeb280ee6c38d5c0b
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2024-06-04 21:45:27 -0300

    bomdia

--- ex016/script.js
+++ ex016/script.js
@@ -3,17 +3,16 @@ function carregar(){
     var img = window.document.getElementById('img')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora}horas.`
     if(hora >= 6 && hora <= 12){
-        //BOM DIA
+        msg.innerHTML = `Agora são ${hora}horas, Bom dia!`
         img.src = 'manhã1.png'
         document.body.style.background = '#E7C000'
     } else if(hora > 12 && hora <= 18){
-        //BOA TARDE
+        msg.innerHTML = `Agora são ${hora}horas, Boa Tarde!`
         img.src = 'tarde1.png'
         document.body.style.background = '#EF9047'
     }else{
-        //BOA NOITE
+        msg.innerHTML = `Agora são ${hora}horas, Boa noite!`
         img.src = 'noite1.png'
         document.body.style.background = '#3F3F3F'
     }
