717cb313a5f3d77eb1de250e55fcf9fa9f155b46
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2024-06-08 17:59:56 -0300

    Create ex021.js

---
+++ exercicios/ex021.js
@@ -0,0 +1,16 @@
+let vetor = [1, 4, 6, 9]
+    vetor.push(8)
+    vetor.push(2)
+    vetor.sort()
+    console.log(vetor)
+    console.log(`Este vetor tem ${vetor.length} Posições.`)
+let pos = vetor.indexOf(2)
+    console.log(`O número 2 está na ${pos} posição.`)
+
+
+
+
+/*for(pos in vetor){
+    console.log(`Na posição ${pos} o valor interno é ${vetor[pos]}`)
+}
+console.log(`o 4º valor é o ${vetor[3]}`)*/
