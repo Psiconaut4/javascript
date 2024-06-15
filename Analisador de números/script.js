8dca049db19b92f79628a3706e48739a462ea22f
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2024-06-15 14:27:57 -0300

    Update AnalisadorDeNúmeros

--- Analisador de números/script.js
+++ Analisador de números/script.js
@@ -21,10 +21,40 @@ function InLista(n, l){
 
 function adicionar(){
     if (IsNumero(num.value) && !InLista(num.value, valores)) {
+        valores.push(Number(num.value))
         let item = document.createElement('option')  
-          item.text = (num.value)
+          item.text = `Valor ${num.value} adicionado.`
           lis.appendChild(item)
+          res.innerHTML = ''
     } else {
         alert('Valor invalido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        alert('Adicione valores para analisar.')
+    } else{
+        var tot = valores.length
+        let maior = valores[0]
+        let menor = valores [0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo foram cadastrados ${tot} valores.</p>`
+        res.innerHTML += `<p>${maior} foi o maior valor encontrado.</p>`
+        res.innerHTML += `<p>${menor} foi o menor valor encontrado.</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores informados é ${media}</p>`
     }
 }
